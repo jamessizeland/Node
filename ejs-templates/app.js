a50d92e63dc8bcb4d6193c5453cb426f34a572a1
James Sizeland <jamessizeland@gmail.com>
2020-11-01 21:00:04 +0000

    adds routing for multiple list pages and layout concept.

--- ejs-templates/app.js
+++ ejs-templates/app.js
@@ -1,58 +1,70 @@
-//establish constants
+//Node & Package Declarations
+/*******************************************************/
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 app.set('view engine', 'ejs'); //set view engine to use Embedded JavaScript Templates for its view engine
 //this assumed a 'views' directory exists and it contains an index.ejs file
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public")); //make public folder accessible for css file
 
+//App Variables
+/*******************************************************/
+let items = []; //list of to do items
+let workItems = [];
 
+//Render each webpage when requested by the browser
+/*******************************************************/
+/*******************************************************/
+
+//Home Route
 app.get("/", function (req, res) {
     const today = new Date();
-    // const currentDay = today.getDay();
-    // const date = today.getDate();
-    // let day = "";
-
-    // switch (currentDay) {
-    //     case 0:
-    //         day = "Sunday";
-    //         break;
-    //     case 1:
-    //         day = "Monday"
-    //         break;
-    //     case 2:
-    //         day = "Tuesday";
-    //         break;
-    //     case 3:
-    //         day = "Wednseday";
-    //         break;
-    //     case 4:
-    //         day = "Thursday";
-    //         break;
-    //     case 5:
-    //         day = "Friday";
-    //         break;
-    //     case 6:
-    //         day = "Saturday";
-    //         break;
-    //     default:
-    //         console.log(`Error: current day is equal to ${currentDay}`);
-    // }
-    var options = {
+    const options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
-    var day = today.toLocaleDateString("en-US", options);
+    const day = today.toLocaleDateString("en-US", options);
     res.render("list", {
-        kindOfDay: day
-    })
+        listTitle: day,
+        toDoList: items
+    }); //render list.ejs
 });
 
-app.post("/", function (req, res) {
-    let newItem = req.body.newItem;
-})
+//Work Route
+app.get("/work", function (req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        toDoList: workItems
+    });
+});
+//About Route
+app.get("/about", function (req, res) {
+    res.render("about");
+});
 
+/*******************************************************/
+//Dynamically respond to post requests by checking the context of the list form submit button name
+app.post("/", function (req, res) {
+    const item = req.body.newItem;
+    console.log("test");
+    console.log(req.body);
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/"); //calls our app.get("/",...)
+    };
+    console.log(item);
+});
+
+//Spin up the server after everything else is declared
+/*******************************************************/
 app.listen(port, function () {
     console.log(`Server started on port ${port}`);
 });
